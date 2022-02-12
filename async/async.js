afd3ee7b24425256a6f42cd229b96188490a6813
Tissue94 <tissue2979@naver.com>
2022-02-13 00:01:35 +0900

    22.02.13

--- async/async.js
+++ async/async.js
@@ -22,7 +22,7 @@ function delay(ms) {
 }
 
 async function getApple() {
-    await delay(1000);
+    await delay(2000);
     return 'apple';
 }
 
@@ -32,9 +32,27 @@ async function getBanana() {
 }
 
 async function pickFruits() {
-    const apple = await getApple();
-    const banana = await getBanana();
+    const applePromise = getApple(); // 병렬적으로 실행하기 위해 프로미스를 활용한다.
+    const bananaPromise = getBanana(); // 동시다발적으로 수행이 가능한 경우 
+    const apple = await applePromise
+    const banana = await bananaPromise
     return `${apple} + ${banana}`;
 }
 
 pickFruits().then(console.log);
+
+// 3. useful Promise APIs
+function pickAllFruits() {
+    // Promise.all() -> 프로미스 배열을 전달하면 모든 프로미스들이 병렬적으로 다 받을때 까지 모아주는 것
+    return Promise.all([getApple(), getBanana()])
+    .then(fruits => fruits.join(' + '));
+}
+
+pickAllFruits().then(console.log)
+
+function pickOnlyOne() {
+    // Promise.race -> 배열에 전달된 프로미스 중에서 가장 먼저 값을 리턴하는 것만 전달
+    return Promise.race([getApple(), getBanana()])
+}
+
+pickOnlyOne().then(console.log)
